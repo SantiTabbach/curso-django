40cb2fe067bdfd050a1fdfedab74bbbda148431d
Santi Tabbach <tabbach97@gmail.com>
2024-10-30 20:13:09 -0300

    Pass data to templates

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project, Task
+from django.shortcuts import render
+
 
 # Create your views here.
-def hello(_req):
-    return HttpResponse("Hola loko")
+
+
+def index(req):
+    title = "Welcome Baquito app"
+    return render(req, "index.html", {"title": title})
+
+
+def hello(_req, username):
+    return HttpResponse(f"<h2>Hola, {username}</h2>")
+
+
+def projects(req):
+    projects = Project.objects.all()
+    return render(req, "projects.html", {"projects": projects})
+
+
+def tasks(req):
+    tasks = Task.objects.all()
+    return render(req, "tasks.html", {"tasks": tasks})
