b7f6bcd4b9e5f7402b20d64130351435655c8055
Santi Tabbach <tabbach97@gmail.com>
2024-11-04 08:40:46 -0300

    Add project form

--- myapp/forms.py
+++ myapp/forms.py
@@ -4,3 +4,7 @@ from django import forms
 class CreateNewTask(forms.Form):
     title = forms.CharField(label="Titulo", max_length=200)
     description = forms.CharField(label="Descripción", widget=forms.Textarea)
+
+
+class CreateNewProject(forms.Form):
+    name = forms.CharField(label="Nombre", max_length=200)
