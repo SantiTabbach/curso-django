1ab0edd4c14d756f9d59884db9441ba19e4a9a13
Santi Tabbach <tabbach97@gmail.com>
2024-11-04 08:41:08 -0300

    Refactorize templates

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from .models import Project, Task
 from django.shortcuts import render, redirect
-from .forms import CreateNewTask
+from .forms import CreateNewTask, CreateNewProject
 
 
 # Create your views here.
@@ -18,21 +18,33 @@ def hello(_req, username):
 
 def projects(req):
     projects = Project.objects.all()
-    return render(req, "projects.html", {"projects": projects})
+    return render(req, "projects/projects.html", {"projects": projects})
 
 
 def tasks(req):
     tasks = Task.objects.all()
-    return render(req, "tasks.html", {"tasks": tasks})
+    return render(req, "projects/tasks.html", {"tasks": tasks})
 
 
 def create_task(req):
     if req.method == "GET":
-        return render(req, "create_task.html", {"form": CreateNewTask})
+        return render(req, "tasks/create_task.html", {"form": CreateNewTask})
     else:
         Task.objects.create(
             title=req.POST["title"],
             description=req.POST["description"],
             project_id=1,
         )
-        return redirect("/tasks/")
+        return redirect("/tasks")
+
+
+def create_project(req):
+    if req.method == "GET":
+        return render(
+            req, "projects/create_project.html", {"form": CreateNewProject}
+        )
+    else:
+        Project.objects.create(
+            name=req.POST["name"],
+        )
+        return redirect("/projects")
